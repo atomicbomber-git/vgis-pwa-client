f2ded45e546498100a4c58f05349244c4334126c
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-09-25 11:17:15 +0700

    Updates.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  dsn: 'https://da686f87b8fe4eb585c1a0b504548f30@o255420.ingest.sentry.io/5440818',
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1,
+});
 
 // Import vue2-google-maps
 import * as VueGoogleMaps from 'vue2-google-maps'
